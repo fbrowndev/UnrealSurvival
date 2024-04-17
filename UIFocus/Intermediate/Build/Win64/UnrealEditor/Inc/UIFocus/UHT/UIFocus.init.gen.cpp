99a51781b87ed3cd15e1284fb0f2f9673c25d906
Keke Brown <fbrowndev@gmail.com>
2024-04-16 20:27:36 -0400

    InitialCommit

---
+++ UIFocus/Intermediate/Build/Win64/UnrealEditor/Inc/UIFocus/UHT/UIFocus.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeUIFocus_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_UIFocus;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_UIFocus()
+	{
+		if (!Z_Registration_Info_UPackage__Script_UIFocus.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/UIFocus",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x87FBC334,
+				0xEE50B142,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_UIFocus.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_UIFocus.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UIFocus(Z_Construct_UPackage__Script_UIFocus, TEXT("/Script/UIFocus"), Z_Registration_Info_UPackage__Script_UIFocus, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x87FBC334, 0xEE50B142));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
