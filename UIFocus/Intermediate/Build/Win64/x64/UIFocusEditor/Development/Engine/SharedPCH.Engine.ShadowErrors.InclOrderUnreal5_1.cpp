1421f707d744d5270fa65184faf502a639d80229
Keke Brown <fbrowndev@gmail.com>
2024-04-17 23:48:54 -0400

    Removed all assets

--- UIFocus/Intermediate/Build/Win64/x64/UIFocusEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.32.31329
-#include "D:/Unity_PortfolioProjects/UnrealSurvival/UIFocus/Intermediate/Build/Win64/x64/UIFocusEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
--- UIFocus/Source/UIFocus/UIFocus.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UIFocus.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UIFocus, "UIFocus" );
